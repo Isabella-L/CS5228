c2fd7c635655b1f54001d7f3d82c093e440c5292
Isabella wsl <isabellalu01@gmail.com>
2025-09-17 01:45:30 +0800

    update

--- cs5228-a1/A1.py
+++ cs5228-a1/A1.py
@@ -24,21 +24,19 @@ def clean(df_cars_dirty):
     
     # clean listing_id
     df_cars_cleaned = df_cars_cleaned[df_cars_cleaned['listing_id'].astype(str).str.isdigit()]
-    print('Number of records after cleaning listing_id: {}'.format(len(df_cars_cleaned)))
     
     # remove type of vehicle unknown
     df_cars_cleaned = df_cars_cleaned[df_cars_cleaned['type_of_vehicle'] != 'unknown']
-    print('Number of records after cleaning type_of_vehicle: {}'.format(len(df_cars_cleaned)))
     
-    # clean invalid year and owner
+    # clean invalid manufactured year 
     df_cars_cleaned = df_cars_cleaned[(df_cars_cleaned['manufactured'] < 2025) & (df_cars_cleaned['manufactured'] > 1900)]
+   
+    # clean invalid no_of_owners
     df_cars_cleaned = df_cars_cleaned[df_cars_cleaned['no_of_owners'] >= 1]
-    print('Number of records after cleaning manufactured year and no_of_owners: {}'.format(len(df_cars_cleaned)))
     
     # merge make shortform
     df_cars_cleaned['make'] = df_cars_cleaned['make'].replace('mercedes', 'mercedes-benz')
     df_cars_cleaned['make'] = df_cars_cleaned['make'].replace('rolls', 'rolls-royce')
-    print('Number of records after cleaning make: {}'.format(len(df_cars_cleaned)))
     
     ### Your code ends here #################################################################
     #########################################################################################
@@ -97,8 +95,42 @@ def extract_facts(df_cars_facts):
 
     #########################################################################################
     ### Your code starts here ###############################################################
+    
+    # first filter out cancelled listing_id
+    df_cars_facts = df_cars_facts[df_cars_facts['listing_id'].astype(str).str.isdigit()]
+    print('Number of records after cleaning listing_id: {}'.format(len(df_cars_facts)))
 
+    
+    # find make = toyota and model = corolla and manufactured < 2015
+    df_toyota_corolla = df_cars_facts[(df_cars_facts['make'] == 'toyota') & (df_cars_facts['model'] == 'corolla') & (df_cars_facts['manufactured'] < 2010)]
+    print('1. Number of toyota corolla manufactured before 2010: {}'.format(len(df_toyota_corolla)))
 
+    # top 3 most common make
+    print("2. ", df_cars_facts['make'].value_counts().head(3))
+
+    # count of most common model with type_of_vehicle = suv
+    df_suv = df_cars_facts[df_cars_facts['type_of_vehicle'] == 'suv']
+    print("3. ", df_suv['model'].value_counts().head(1))
+
+    # car's make and price with highest overall price and power < 60
+    df_power_60 = df_cars_facts[df_cars_facts['power'] <= 60]
+    idx_max_price = df_power_60['price'].idxmax()
+    print("4. ", df_power_60.loc[idx_max_price, ['make', 'price']])
+
+    # entry where type_of_vehicle = med size sedan and highest power-to-engine_cap ratio
+    midsize = df_cars_facts[df_cars_facts['type_of_vehicle'] =='mid-sized sedan'].copy()
+    midsize['power_to_engine'] = round(midsize['power'] / midsize['engine_cap'],2)
+    make, model, manufactuered, power_to_engine = midsize.loc[midsize['power_to_engine'].idxmax(),['make','model','manufactured','power_to_engine']]
+    print("5. ", make, model, manufactuered, power_to_engine)
+    # print(midsize.loc[midsize['power_to_engine'].idxmax()])
+
+    # pearson correlation between price and mileage
+    print("6. Correlation between price and mileage: {:.2f}".format(
+        df_cars_facts['price'].corr(df_cars_facts['mileage']),
+    ))
+    print("   Correlation between price and engine_cap: {:.2f}".format(
+        df_cars_facts['price'].corr(df_cars_facts['engine_cap'])
+    ))
 
     ### Your code ends here #################################################################
     #########################################################################################
